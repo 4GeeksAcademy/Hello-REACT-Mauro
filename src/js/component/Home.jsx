c975c614a88f3daf3320b5f68c53a6f6a3fac015
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-01-17 15:06:51 +0000

    ThirdCommit

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from "react"; // Import React
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+
+// 6 & 7 >> Import Element.jsx to the Home and Use it
+import NavBar from "./NavBar.jsx"; // Import Element with "export default"
+
+import { Footer } from "./Footer.jsx"; // Import Element with "export const"
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
+			<NavBar />
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -14,11 +20,8 @@ const Home = () => {
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			<Footer />
 		</div>
 	);
 };
